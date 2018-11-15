2edf786a231735e619c97d6bbc6c97956d8ac379
rc941t <rc941t@att.com>
2018-11-15 16:27:44 -0500

    create the filebeat and entrypoint to logs docker data

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,15 @@
 FROM docker.elastic.co/beats/filebeat:6.4.3
 
-LABEL MAINTAINER="roberto cardenas" 
+LABEL MAINTAINER="roberto cardenas"
 LABEL MAINTAINER_EMAIL="rcardenas20@gmail.com"
 
 ENV FILEBEAT_HOME /usr/share/filebeat
-
-USER filebeat
 ADD filebeat.yml ${FILEBEAT_HOME}/filebeat.yml
-USER root
 
-ADD  entrypoint.sh /entrypoint.sh
-RUN  chmod +x /entrypoint.sh
+USER root
+RUN chown root:filebeat ${FILEBEAT_HOME}/filebeat.yml
+ADD entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT  ["/entrypoint.sh"]
 CMD ["start"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,21 +3,27 @@ set -e
 
 if [ "$1" = 'start' ]; then
 
+    mkdir -p ${LOGS_PATH}
+
     getRunningContainers() {
     curl --no-buffer -s -XGET --unix-socket /tmp/docker.sock http:/containers/json | python -c "
-import json, sys
+import json, sys, datetime
 containers=json.loads(sys.stdin.readline())
 for container in containers:
-    print(container)
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    logs_path = '${LOGS_PATH}' + timestamp + '.json'
+    with open(logs_path, 'a+') as logfile:
+        json.dump(container, logfile)
+        logfile.write('\n')
+        logfile.close()
 "
-    }
+}
 
-    ${FILEBEAT_HOME}/filebeat -e -v &
+    cd ${FILEBEAT_HOME}
+    ./filebeat -e -c filebeat.yml &
     while true; do
-        CONTAINERS=`getRunningContainers`
-        echo "$CONTAINERS"
-        echo ""
-        sleep 5
+        getRunningContainers
+        sleep 10
     done
 else
   exec "$@"
